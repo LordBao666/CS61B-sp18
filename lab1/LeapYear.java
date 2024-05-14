6d6060db87283aade14f03f1b096e300397c645d
LordBao666 <916900021@qq.com>
2024-05-14 11:43:13 +0800

    Finish Leap year of Lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,7 +1,16 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author LordBao666
  */
 public class LeapYear {
+
+
+    /** Check a year is leap or not */
+    public static boolean isLeapYear(int year){
+        if(year % 400==0 || (year % 4==0 && year % 100 !=0)){
+            return true;
+        }    
+        return false;
+    }
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
