f1b35e88b35f75807bf46014977e5deda3141345
LordBao666 <916900021@qq.com>
2024-05-22 22:17:59 +0800

    Refine proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,154 +2,177 @@
  * @Author Lord_Bao
  * @Date 2024/5/22 17:41
  * @Version 1.0
- *
+ * <p>
  * I implemented ArrayDeque by using circular array as
  * https://sp18.datastructur.es/materials/proj/proj1a/proj1a mentioned
  */
 public class ArrayDeque<T> {
-    private Object []items;
+    private Object[] items;
     private int size;
-    private int nextFirst;//the position to add item in backward direction
-    private int nextLast;//the position to add item in forward direction
+    private int nextFirst; //the position to add item in backward direction
+    private int nextLast; //the position to add item in forward direction
     private int capacity;
 
     public ArrayDeque() {
-        capacity=8;//The default capacity is 8
-        size=0;
+        capacity = 8; //The default capacity is 8
+        size = 0;
         //nextFirst does not have to be 3 and nextLast does not have to be 4
         //as long as nextFirst is just in front of nextLast.For instance,
         //nextFirst can be 2,and nextLast can be 3 as well
-        nextFirst=3;
-        nextLast=4;
-        items=new Object[capacity];
+        nextFirst = 3;
+        nextLast = 4;
+        items = new Object[capacity];
     }
 
-    public int size(){
+    public int size() {
         return size;
     }
-    public boolean isEmpty(){
-        return size==0;
-    }
-    public boolean isFull(){
-        return size==capacity;
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 
-    /**though we can not initialize generic array, we can pass generic array
+    public boolean isFull() {
+        return size == capacity;
+    }
+
+    /**
+     * though we can not initialize generic array, we can pass generic array
      * Here we add these data from the end.
-     * */
+     */
     @SafeVarargs
-    public final void  add(T... data){
-            for(T ele:data){
-                addLast(ele);
-            }
+    public final void add(T... data) {
+        for (T ele : data) {
+            addLast(ele);
+        }
     }
 
-    public void addFirst(T data){
-        if(isFull()){
+    public void addFirst(T data) {
+        if (isFull()) {
             expand();
         }
-        items[nextFirst]=data;
-        nextFirst=(nextFirst-1+capacity)%capacity;
+        items[nextFirst] = data;
+        nextFirst = (nextFirst - 1 + capacity) % capacity;
         size++;
     }
 
-    public void addLast(T data){
-        if(isFull()){
+    public void addLast(T data) {
+        if (isFull()) {
             expand();
         }
-        items[nextLast]=data;
-        nextLast=(nextLast+1)%capacity;
+        items[nextLast] = data;
+        nextLast = (nextLast + 1) % capacity;
         size++;
     }
 
 
-    private void resize(int newCapacity){
-        Object [] newItems = new Object[newCapacity];
-        int p = (nextFirst+1)%capacity;//p is used to scan the original items
-        int q = p%newCapacity;//q is used to scan the new items
-        for (int i=0;i<size;i++){//The number of items copied is size
-            newItems[q]=items[p];
-            items[p]=null;//avoid loitering
-            p=(p+1)%capacity;//move p
-            q=(q+1)%newCapacity;//move q
+    private void resize(int newCapacity) {
+        Object[] newItems = new Object[newCapacity];
+        int p = (nextFirst + 1) % capacity; //p is used to scan the original items
+        int q = p % newCapacity; //q is used to scan the new items
+        for (int i = 0; i < size; i++) { //The number of items copied is size
+            newItems[q] = items[p];
+            items[p] = null; //avoid loitering
+            p = (p + 1) % capacity; //move p
+            q = (q + 1) % newCapacity; //move q
         }
-//        nextFirst=nextFirst; //nextFirst doesn't change
-        nextLast=q;
-        capacity=newCapacity;
-        items=newItems;
+        // nextFirst=nextFirst; //nextFirst doesn't change
+        nextLast = q;
+        capacity = newCapacity;
+        items = newItems;
     }
 
-    /**expand the items in the ratio of 1.5*/
-    private void expand(){
-        int newCapacity=capacity+(capacity>>1);
+    /**
+     * expand the items in the ratio of 1.5
+     */
+    private void expand() {
+        int newCapacity = capacity + (capacity >> 1);
         resize(newCapacity);
     }
 
-    /**Prints the items in the deque from first to last, separated by a space*/
-    public void printDeque(){
+    /**
+     * Prints the items in the deque from first to last, separated by a space
+     */
+    public void printDeque() {
         // p is the first element
-        int p = (nextFirst+1)%capacity;
+        int p = (nextFirst + 1) % capacity;
         int count = size;
         System.out.print("ArrayDeque{ ");
-        while (count>0){
-            System.out.print(items[p]+" ");
+        while (count > 0) {
+            System.out.print(items[p] + " ");
             count--;
-            p=(p+1)%capacity;
+            p = (p + 1) % capacity;
         }
         System.out.print("}");
         System.out.println();
     }
 
-    /** check if the items to be shrunk:
-     *  if the ratio of usage is below .25 and capacity>=16, return true,
-     *  otherwise,return false.
-     * */
-    private boolean isShrinkAble(){
-        return (size * 4 < capacity) && ( capacity>=16);
+    /**
+     * check if the items to be shrunk:
+     * if the ratio of usage is below .25 and capacity>=16, return true,
+     * otherwise,return false.
+     */
+    private boolean isShrinkAble() {
+        final int threshold = 16;
+        return (size * 4 < capacity) && (capacity >= threshold);
     }
 
-    /**halve the items*/
-    private void shrink(){
-        int newCapacity=(capacity>>1);
+    /**
+     * halve the items
+     */
+    private void shrink() {
+        int newCapacity = (capacity >> 1);
         resize(newCapacity);
     }
 
-    /**Removes and returns the item at the front of the deque. If no such item exists, returns null.*/
+    /**
+     * Removes and returns the item at the front of the deque. If no such item exists, returns null.
+     */
     @SuppressWarnings("unchecked")
-    public T removeFirst(){
-        if(isEmpty()) return null;
-        int first = (nextFirst+1)%capacity;
-        T ele= (T)items[first];
-        items[first]=null;//avoid loitering
-        nextFirst=first;
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        int first = (nextFirst + 1) % capacity;
+        T ele = (T) items[first];
+        items[first] = null; //avoid loitering
+        nextFirst = first;
         size--;
-        if(isShrinkAble()){
+        if (isShrinkAble()) {
             shrink();
         }
         return ele;
     }
 
-    /**Removes and returns the item at the back of the deque. If no such item exists, returns null.*/
+    /**
+     * Removes and returns the item at the back of the deque. If no such item exists, returns null.
+     */
     @SuppressWarnings("unchecked")
-    public T removeLast(){
-        if(isEmpty()) return null;
-        int last = (nextLast-1+capacity)%capacity;
-        T ele= (T)items[last];
-        items[last]=null;//avoid loitering
-        nextLast=last;
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        int last = (nextLast - 1 + capacity) % capacity;
+        T ele = (T) items[last];
+        items[last] = null; //avoid loitering
+        nextLast = last;
         size--;
-        if(isShrinkAble()){
+        if (isShrinkAble()) {
             shrink();
         }
         return ele;
     }
 
-    /**Gets the item at the given index, where 0 is the front,
+    /**
+     * Gets the item at the given index, where 0 is the front,
      * 1 is the next item, and so forth. If no such item exists,
-     * returns null. Must not alter the deque!*/
+     * returns null. Must not alter the deque!
+     */
     @SuppressWarnings("unchecked")
-    public T get(int index){
-        if(index>=size || index<0) return null;
-        return (T)(items[(nextFirst+1+index)%capacity]);
+    public T get(int index) {
+        if (index >= size || index < 0) {
+            return null;
+        }
+        return (T) (items[(nextFirst + 1 + index) % capacity]);
     }
 }
--- proj1a/ArrayDequeTest.java
+++ proj1a/ArrayDequeTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 public class ArrayDequeTest {
     @Test
-    public void testAddFirstAndAddLast(){
+    public void testAddFirstAndAddLast() {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
         deque.addFirst(2);
         deque.addFirst(1);
@@ -19,18 +19,18 @@ public class ArrayDequeTest {
     }
 
     @Test
-    public void  testAdd(){
+    public void testAdd() {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
-        deque.add(1,3,5,7,9);
+        deque.add(1, 3, 5, 7, 9);
         deque.printDeque();
     }
 
     //you had better run the code in debug mode,and the Java
     //Visualizer can tell you everything!!!
     @Test
-    public void  testExpand(){
+    public void testExpand() {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
-        deque.add(1,2,3,4,5,6,7,8);
+        deque.add(1, 2, 3, 4, 5, 6, 7, 8);
         deque.printDeque();
         System.out.println(deque.size());
         deque.addLast(9);
@@ -39,14 +39,12 @@ public class ArrayDequeTest {
     }
 
     @Test
-    public void  testRemove(){
+    public void testRemove() {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
-        deque.add(1,2,3,4,5,6,7,8);
+        deque.add(1, 2, 3, 4, 5, 6, 7, 8, 9);
         deque.printDeque();
         System.out.println(deque.size());
         System.out.println(deque.removeFirst());
-        System.out.println(deque.removeFirst());
-        System.out.println(deque.removeLast());
         System.out.println(deque.removeLast());
         deque.printDeque();
         System.out.println(deque.size());
@@ -55,19 +53,19 @@ public class ArrayDequeTest {
     //you had better run the code in debug mode and find out
     //when the array is shrunk.
     @Test
-    public void  testShrink(){
+    public void testShrink() {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
-        deque.add(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
-        for(int i=0;i<17;i++){
+        deque.add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        for (int i = 0; i < 17; i++) {
             deque.removeLast();
         }
         System.out.println(deque.size());
     }
 
     @Test
-    public void testGet(){
+    public void testGet() {
         ArrayDeque<Integer> deque = new ArrayDeque<>();
-        deque.add(1,2,3,4);
+        deque.add(1, 2, 3, 4);
         System.out.println(deque.get(-1));
         System.out.println(deque.get(0));
         System.out.println(deque.get(1));
